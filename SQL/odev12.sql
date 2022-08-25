1fb2264f9e69502037cfa6ff05404b8df09f3ab9
nalcaciomer <nalcaciomer@outlook.com>
2022-08-25 15:25:35 +0300

    Soru 4 eklendi

--- SQL/odev12.sql
+++ SQL/odev12.sql
@@ -6,6 +6,8 @@
 -- 3)film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
 --   payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
 
+-- 4)payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+
 -- 1)
 SELECT COUNT(*) FROM film
 WHERE length > (SELECT AVG(length) FROM film);
@@ -24,6 +26,3 @@ FROM payment
 INNER JOIN customer ON customer.customer_id = payment.customer_id
 GROUP BY payment.customer_id, first_name, last_name 
 ORDER BY count_payment DESC;
-
-
-
